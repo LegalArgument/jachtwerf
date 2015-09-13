e355e676482af7fd66a0199e03fade28e769bec8
Legal Argument <jillversluys@gmail.com>
2015-09-13 19:30:00 +0200

    geraamte gezet

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,35 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        return $this->render('simpel/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    public function tarievenAction()
+    {
+        return $this->render('simpel/tarieven.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    public function dewerfAction(Request $request)
+    {
+        return $this->render('simpel/werf.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    public function bergingAction()
+    {
+        return $this->render('simpel/berging.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    public function contactAction()
+    {
+        return $this->render('simpel/contact.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
