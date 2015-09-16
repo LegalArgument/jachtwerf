4c05b73ac175f07d480240962d4a1a18eb246111
Legal Argument <jillversluys@gmail.com>
2015-09-16 12:54:15 +0200

    clean code

--- web/app.php
+++ web/app.php
@@ -5,6 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
+//TODO enable APC
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
 // in order to prevent cache key conflicts with other applications
